6db5bc04b4a7659fb4607851a8d27c4e0db553de
rogeriofelipevieira <rferpa28@gmail.com>
2024-11-26 12:25:21 -0300

    Ajuste LoginErro

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -4,8 +4,6 @@ describe('Página de login', () => {
     cy.get('[data-test="login-button"]').click();
 })
   it('Deve preencher os campos do login incorretamente e exibir mensagens de erro ao usuário na página', () => {
-    cy.loginErro('rogerio felipe', '123');
-    cy.contains('Por favor, verifique o email digitado').should('be.visible')
-    cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres').should('be.visible')
+    cy.loginErro('rogerio felipe', '123')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,6 +7,7 @@ cy.get('[data-test="submit-button"]').click();
 Cypress.Commands.add('loginErro', (email, senha) => { 
 cy.get('[data-test="input-loginEmail"]').type(email);
 cy.get('[data-test="input-loginPassword"]').type(senha);
+cy.get('[data-test="submit-button"]').click();
 cy.contains('Por favor, verifique o email digitado').should('be.visible')
 cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres').should('be.visible')
 })
