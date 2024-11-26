90e66b6f2c8f897cce4f2b2c5a7c2ddcab34a6e7
rogeriofelipevieira <rferpa28@gmail.com>
2024-11-26 12:13:14 -0300

    ajuste loginErro

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -1,11 +1,11 @@
 describe('Página de Login', () => {
-    beforeEach(() => {
-        cy.visit('https://adopet-frontend-cypress.vercel.app/');
-        cy.get('[data-test="login-button"]').click();
-    })
+  beforeEach(() => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="login-button"]').click();
+})
     
-    it('Deve preencher os campos de login corretamente e autenticar o usuário na pagina', () => {
-      cy.login('rogeriofelipe@email.com', 'Senha123')
+  it('Deve preencher os campos de login corretamente e autenticar o usuário na pagina', () => {
+    cy.login('rogeriofelipe@email.com', 'Senha123')
 
-    })
   })
+})
