677347b081cdcc605d728c2c6c0c283999540fa3
rogeriofelipevieira <rferpa28@gmail.com>
2024-11-26 01:08:15 -0300

    addComandos

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -5,9 +5,7 @@ describe('Página de Login', () => {
     })
     
     it('Deve preencher os campos de login corretamente e autenticar o usuário na pagina', () => {
-     
-      cy.get('[data-test="input-loginEmail"]').type('rogeriofelipe@email.com');
-      cy.get('[data-test="input-loginPassword"]').type('Senha123');
-      cy.get('[data-test="submit-button"]').click();
+      cy.login('rogeriofelipe@email.com', 'Senha123')
+
     })
   })
